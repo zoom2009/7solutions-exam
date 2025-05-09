baedb5d3001010ad33bd38b6bc2aae1561aef91b
zoom2009 <afumagic3@gmail.com>
2025-05-09 20:33:30 +0700

    add note for reason why using prefetch and useQuery

--- src/app/data-api/_components/DisplayUsers.tsx
+++ src/app/data-api/_components/DisplayUsers.tsx
@@ -4,6 +4,9 @@ import { useQuery } from "@tanstack/react-query"
 import { getUsers } from "../queries"
 import transformUsers from "../_functions/transformUsers"
 
+// NOTE: normally this ui no need to use client but for complex ui will be client component
+// This is Example way to keep good seo for data fetching and ui activity with user (like onClick)
+
 const DisplayUsers = () => {
   const { data } = useQuery({
     queryKey: ['users'],
