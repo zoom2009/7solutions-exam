d9e0ce1cc51d8635b85169d9d3072bb949f7c73b
zoom2009 <afumagic3@gmail.com>
2025-05-09 17:38:57 +0700

    create getUsers fn

---
+++ src/app/data-api/queries.ts
@@ -0,0 +1,85 @@
+type TUser = {
+  id: number
+  firstName: string
+  lastName: string
+  maidenName: string
+  age: number
+  gender: string
+  email: string
+  phone: string
+  username: string
+  password: string
+  birthDate: string
+  image: string
+  bloodGroup: string
+  height: number
+  weight: number
+  eyeColor: string
+  hair: {
+    color: string
+    type: string
+  }
+  ip: string
+  address: {
+    address: string
+    city: string
+    state: string
+    stateCode: string
+    postalCode: string
+    coordinates: {
+      lat: number
+      lng: number
+    }
+    country: string
+  }
+  macAddress: string
+  university: string
+  bank: {
+    cardExpire: string
+    cardNumber: string
+    cardType: string
+    currency: string
+    iban: string
+  }
+  company: {
+    department: string
+    name: string
+    title: string
+    address: {
+      address: string
+      city: string
+      state: string
+      stateCode: string
+      postalCode: string
+      coordinates: {
+        lat: number
+        lng: number
+      }
+      country: string
+    }
+  }
+  ein: string
+  ssn: string
+  userAgent: string
+  crypto: {
+    coin: string
+    wallet: string
+    network: string
+  }
+  role: string
+}
+
+type TUserResult = {
+  limit: number
+  skip: number
+  total: number
+  users: TUser[]
+}
+
+export const getUsers = async (): Promise<TUserResult> => {
+  const response = await fetch("https://dummyjson.com/users")
+  const responseJson = await response.json()
+  // for testing loading is render and seo still work
+  // await new Promise((resolve) => setTimeout(resolve, 3000))
+  return responseJson
+}
